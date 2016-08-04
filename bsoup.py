bbe30968e4b51dcf3c43b9f162336de8c2aec406
lujx <13426136199@189.cn>
2016-08-04 16:45:42 +0800

    use requests get info

--- bsoup.py
+++ bsoup.py
@@ -4,27 +4,25 @@ import urllib
 import urllib2
 import lxml
 from bs4 import BeautifulSoup
-
-'''
-#coding:utf-8
+import requests
 import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
-'''
-
-
 def execute():
-    url = "http://www.biquge.la/xiaoshuodaquan/"
-    req = urllib2.Request(url)
-    response = urllib2.urlopen(req)
-    html = response.read()
-    soup = BeautifulSoup(html,"lxml")
+
+    baseurl = "http://www.biquge.la/xiaoshuodaquan/"
+    noveinfo = {}
+    result = requests.get(baseurl)
+    result.encoding = 'GBK'
+    print result.status_code
+    #print result.url
+    #print result.text
+
+    soup = BeautifulSoup(result.text,"lxml")
 
     print soup.prettify()
     print soup.title
 
-    #print soup.select('div[class="nav"]')[0].encode("utf-8")
     print soup.select('div[class="novellist"]')[0].encode("utf-8")
 
 if __name__ == '__main__':
     execute()
+
